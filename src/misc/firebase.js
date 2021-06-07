e96ce43ae369b502eacdb1c5869ba5250813e3cb
kapil3k <kapil2khatri123@gmail,com>
2021-06-07 11:23:50 +0530

    project started

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyBkAijJ7zECZofOrJJMqbjQedlMqn2-6DA",
+    authDomain: "new-chat-app-80c9c.firebaseapp.com",
+    projectId: "new-chat-app-80c9c",
+    storageBucket: "new-chat-app-80c9c.appspot.com",
+    messagingSenderId: "624418488061",
+    appId: "1:624418488061:web:90655cb1c1478718704894"
+  };
+
+
+  const app = firebase.initializeApp(config);
